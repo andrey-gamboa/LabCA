f8979642a6c250d7b1c90e9376ff48538b249b41
Andrey Gamboa <57918550+andrey-gamboa@users.noreply.github.com>
2026-02-03 16:59:07 -0600

    Create install-apache.sh

---
+++ install/install-apache.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+a2enmod proxy proxy_http rewrite
+cp /tmp/LabCA/apache/000-default.conf /etc/apache2/sites-available/000-default.conf
+systemctl restart apache2
