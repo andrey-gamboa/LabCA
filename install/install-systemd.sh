e4b87a8f2faee40a6f55ac6f7058eaba12004e03
Andrey Gamboa <57918550+andrey-gamboa@users.noreply.github.com>
2026-02-03 21:18:23 -0600

    Update install-systemd.sh

--- install/install-systemd.sh
+++ install/install-systemd.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-cp /tmp/LabCA/systemd/ca-web.service /etc/systemd/system/ca-web.service
-
+cp /opt/ca-lab/install/ca-web.service /etc/systemd/system/ca-web.service
 systemctl daemon-reload
 systemctl enable --now ca-web
